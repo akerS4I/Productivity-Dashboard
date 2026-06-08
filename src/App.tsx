5ed5fa4d7450d1f448c2b7dd4320842bed960f6e
akerS4I <birdblack283474@gmail.com>
2026-06-09 00:22:28 +0500

    feat(theme): integrate shadcn theme provider with context and useTheme hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import type { Task } from "./types";
+import { ThemeProvider } from "@/components/theme-provider";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Badge } from "./components/ui/badge";
 
 function App() {
   const INITIAL_TASKS: Task[] = [
@@ -17,14 +26,26 @@ function App() {
       },
     },
     {
-      id: "task-1",
+      id: "task-2",
       title: "Plan the development process and tech to use",
       isCompleted: true,
       priority: "medium",
-      tags: ["planning", "architecture"],
+      tags: ["architecture"],
       category: {
         id: "cat-plan",
-        name: "Planning",
+        name: "Development",
+        color: "#10b981",
+      },
+    },
+    {
+      id: "task-3",
+      title: "Watch new Recrent video",
+      isCompleted: false,
+      priority: "low",
+      tags: ["youtube"],
+      category: {
+        id: "cat-plan",
+        name: "entertainment",
         color: "#10b981",
       },
     },
@@ -32,20 +53,40 @@ function App() {
 
   const [tasks, setTasks] = useState<Task[]>(INITIAL_TASKS);
 
+  const priorityColor = (priority: string) => {
+    switch (priority) {
+      case "high":
+        return "destructive";
+      case "medium":
+        return "default";
+      case "low":
+        return "secondary";
+      default:
+        return "ghost";
+    }
+  };
   return (
-    <>
-      <h1>Tasks:</h1>
-      {tasks.map((task) => (
-        <div key={task.id} className="task-card" style={{border: '1px solid', margin: 10, padding: 10}}>
-          <h2>{task.title}</h2>
-          <span
-            style={{ backgroundColor: task.category.color, color: "black" }}
-          >
-            {task.category.name}
-          </span>
-        </div>
-      ))}
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <>
+        <h1 className="mt-5 text-center text-4xl font-extrabold tracking-tight text-balance">
+          Tasks
+        </h1>
+        {tasks.map((task) => (
+          <Card key={task.id} className="mx-auto w-full max-w-sm my-4">
+            <CardHeader>
+              <CardTitle>{task.title}</CardTitle>
+              <CardDescription>{task.tags.join(", ")}</CardDescription>
+            </CardHeader>
+            <CardFooter>
+              Priority:
+              <Badge variant={priorityColor(task.priority)} className="ml-2">
+                {task.priority}
+              </Badge>
+            </CardFooter>
+          </Card>
+        ))}
+      </>
+    </ThemeProvider>
   );
 }
 
